6a6597bae23df8e4fe79343d742f1f9b0f4f2440
AndreLars <andrelarsdacunha@gmail.com>
2021-08-06 23:53:36 -0300

    git init

---
+++ src/main/java/br/gov/sp/fatec/lojadediscos/LojaDeDiscosApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.lojadediscos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDeDiscosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDeDiscosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/lojadediscos/LojaDeDiscosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.lojadediscos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDeDiscosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
